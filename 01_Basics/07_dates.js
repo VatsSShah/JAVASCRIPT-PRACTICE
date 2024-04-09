f0ba5b3b7e1c99afa3aa979c5a0da557bff919c1
VatsSShah <130391685+VatsSShah@users.noreply.github.com>
2024-04-09 08:59:53 +0000

    js basics

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,10 @@
+let myDate = new Date()
+console.log(myDate. toString());
+console.log(myDate. toDateString());
+console.log(myDate. toLocaleString());
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024, 0, 23, 5, 3);
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date("2024-01-14")
