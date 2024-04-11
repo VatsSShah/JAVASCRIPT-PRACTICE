ae50333d8480428592781192dbca91b8b5758797
VatsSShah <130391685+VatsSShah@users.noreply.github.com>
2024-04-11 07:02:28 +0000

    JS CONCEPTS

--- 04_controlFlow/02_ExecutionContextandLexicalEnv.js
+++ 04_controlFlow/02_ExecutionContextandLexicalEnv.js
@@ -3,3 +3,12 @@
 //ye jo imaginary container hai we call that container execution context
 
 //Lexical Environment- ek chart hota hai jisme likha hota hai ki aapka particular function kin cheezo ko access kar sakhta hai and kinko nahi, matlab it holds scope and scope chain
+
+//How to copy reference values: by using the spread operator 
+var a = [1, 2, 3, 4, 5];
+var b = [...a] 
+var c = [1,2,3,4,5,6,7,8,9]
+var d = [...c]
+//copying values of an object 
+var obj = {name: "vats"};
+var copyObj = {...obj};
---
+++ 04_controlFlow/03_Truthy&Falsy.js
@@ -0,0 +1,19 @@
+//js mein kuch bhi likho woh mainly 2 types mein se kisi ek type ko belong karte hai aka Truthy and Falsy
+//falsy values yeh hai: 0, false, undefined, null, NaN, document.all
+//truthy values
+if (7){
+    console.log("hey");
+}
+else{
+    console.log("hello");
+}
+
+//switch case statement
+switch (1) {
+    case 1:
+        
+        break;
+
+    default:
+        break;
+}
